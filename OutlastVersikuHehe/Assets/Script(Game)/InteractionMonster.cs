d072042e11910dc19bda301e8750c6777d3336c7
Haidar <haidar.a.tn@gmail.com>
2018-05-19 11:05:53 +0700

    OutlastProgress1
    Build Environment Horror + Flashlight

---
+++ OutlastVersikuHehe/Assets/Script(Game)/InteractionMonster.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionMonster : MonoBehaviour {
+    public bool isGazed;
+
+    float time = 0;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isGazed)
+        {
+            time += Time.deltaTime;
+
+            if (time > 3)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        else
+        {
+            time = 0;
+        }
+    }
+
+    public void Gazed() {
+        isGazed = true;
+    }
+
+    public void UnGazed()
+    {
+        isGazed = false;
+    }
+}
---
+++ OutlastVersikuHehe/Assets/Script(Game)/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+    }
+
+    void Move()
+    {
+        if(Input.acceleration.x <= -0.2f)
+        {
+            transform.position = transform.position + Camera.main.transform.forward * -speed * Time.deltaTime;
+        }
+
+        if (Input.acceleration.x >= 0.2f)
+        {
+            transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+        }
+    }
+}
